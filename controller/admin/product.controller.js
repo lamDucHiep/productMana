e6339e7cb9f4d309966561fd0a6a8b859d1063cd
LazyDev <lamhiep2601@gmail.com>
2024-07-01 18:09:17 +0700

    take data and print on view

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../models/products.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang quan li san pham",
+    products: products,
   });
 };
