83ffffedefcc4bb25166ac5c2a12d21d26696c3c
LazyDev <lamhiep2601@gmail.com>
2024-07-01 18:29:58 +0700

    filter by status - backend

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -2,9 +2,14 @@ const Product = require("../../models/products.model");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  const find = {
     deleted: false,
-  });
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang quan li san pham",
