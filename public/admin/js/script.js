4da867ff8a8c4f97b082de42d2ca0dfa82034cdc
LazyDev <lamhiep2601@gmail.com>
2024-07-04 07:53:59 +0700

    filter by status - frontend

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,23 @@
-console.log("OK");
+//button status
+const listButtonStatus = document.querySelectorAll("[btn-status]");
+
+if (listButtonStatus.length > 0) {
+  let url = new URL(window.location.href);
+  listButtonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("btn-status");
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url.href;
+    });
+  });
+  const statusCurrent = url.searchParams.get("status") || "";
+  const buttonCurrent = document.querySelector(
+    `[btn-status="${statusCurrent}"]`
+  );
+  buttonCurrent.classList.add("active");
+}
+//end button status
